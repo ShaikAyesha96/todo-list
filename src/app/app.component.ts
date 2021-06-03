8f9e3e98aa942bb9bb7f738209484e305a5a4a0f
Ayesha Shaik <ayeshajob124@gmail.com>
2021-06-03 12:59:20 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { stringify } from '@angular/compiler/src/util';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Todo-List-app';
+  
+
+ 
+}
